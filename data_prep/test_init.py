7f0be61989e33c1e4a54660be13a494da18faed4
janmesh-007 <janmesh007@icloud.com>
2020-12-07 21:40:14 +0530

    data prep update

--- data_prep/test_init.py
+++ data_prep/test_init.py
@@ -10,6 +10,7 @@ parser.add_argument("--keypoints", type=str, help="keypoint directory")
 parser.add_argument("--frame_dir", type=str, help='frame directory')
 parser.add_argument("--save_dir", type=str, help='save directory')
 parser.add_argument("--display", help='display the output', action="store_true")
+parser.add_argument("--no_save", help='not save the output', action="store_true")
 
 args = parser.parse_args()
 
@@ -18,10 +19,10 @@ savedir = args.save_dir
 
 if not os.path.exists(savedir):
     	os.makedirs(savedir)
-if not os.path.exists(savedir + '/train_label'):
-	os.makedirs(savedir + '/train_label')
-if not os.path.exists(savedir + '/train_img'):
-	os.makedirs(savedir + '/train_img')
+if not os.path.exists(savedir + '/test_label'):
+	os.makedirs(savedir + '/test_label')
+if not os.path.exists(savedir + '/test_img'):
+	os.makedirs(savedir + '/test_img')
  
 img_path = os.path.join(args.frame_dir, "*.png")
 imgs = glob.glob(img_path)
@@ -38,16 +39,18 @@ for f in range(len(imgs)):
     output_frame = np.zeros((height, width, 3), np.uint8)
     output_frame.fill(255)
     display_skleton(output_frame, posepts, facepts, r_handpts, l_handpts)
-    out_sk = resize_scale(output_frame, white_bg=True)
-    out_frame = resize_scale(_frame)
+    scale_n, translate_n = resize_scale(output_frame)
+    out_sk = fix_image(scale_n, translate_n,output_frame)
+    out_frame = fix_image(scale_n, translate_n,_frame)
     _fn = "frame_"+'{:0>12}'.format(f)+".png"
-    _filepath_label = os.path.join(savedir, "train_label")
+    _filepath_label = os.path.join(savedir, "test_label")
     filename_label = os.path.join(_filepath_label, _fn)
-    _filepath_img = os.path.join(savedir, "train_img")
+    _filepath_img = os.path.join(savedir, "test_img")
     filename_img = os.path.join(_filepath_img, _fn)
     print("Processing frame: ", f)
-    cv2.imwrite(filename_label, out_sk)
-    cv2.imwrite(filename_img, out_frame)
+    if not args.no_save:
+        cv2.imwrite(filename_label, out_sk)
+        cv2.imwrite(filename_img, out_frame)
     if args.display:
         cv2.imshow("Skleton Frame", out_sk)
      
