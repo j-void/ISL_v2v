e11812bdc07b403b3ac20790293d6904d297d02b
janmesh-007 <janmesh007@icloud.com>
2020-12-15 20:55:05 +0530

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -354,16 +354,16 @@ class NLayerDiscriminator(nn.Module):
         self.n_layers = n_layers
         self.addname = addname
 
-        kw = 4
+        kw = 1
         padw = int(np.ceil((kw-1.0)/2))
-        sequence = [[nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]]
+        sequence = [[nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=1, padding=padw), nn.LeakyReLU(0.2, True)]]
 
         nf = ndf
         for n in range(1, n_layers):
             nf_prev = nf
             nf = min(nf * 2, 512)
             sequence += [[
-                nn.Conv2d(nf_prev, nf, kernel_size=kw, stride=2, padding=padw),
+                nn.Conv2d(nf_prev, nf, kernel_size=kw, stride=1, padding=padw),
                 norm_layer(nf), nn.LeakyReLU(0.2, True)
             ]]
 
