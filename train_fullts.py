8eb4cbaecef458cd27ce681bb45ea75c86770f0f
janmesh-007 <janmesh007@icloud.com>
2020-12-15 13:30:05 +0530

    Update train_fullts.py

--- train_fullts.py
+++ train_fullts.py
@@ -4,7 +4,6 @@ import time
 from collections import OrderedDict
 
 from numpy.lib import utils
-from torch._C import uint8
 from options.train_options import TrainOptions
 from data.data_loader import CreateDataLoader
 from models.models import create_model_fullts
@@ -73,9 +72,9 @@ for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
                     Variable(data['next_image']), Variable(cond_zeros), infer=True)
 
             if total_steps % 100 == 0:
-                gen_img = util.tensor2im(generated[0].data[0])[:,:1024,:].astype(uint8)
+                gen_img = util.tensor2im(generated[0].data[0])[:,:1024,:].astype(np.uint8)
                 targets = torch.cat((data['image'], data['next_image']), dim=3)
-                real_img = util.tensor2im(targets[0])[:,:1024,:].astype(uint8)
+                real_img = util.tensor2im(targets[0])[:,:1024,:].astype(np.uint8)
                 lhpts_gen, rhpts_gen = hand_utils.get_keypoints(gen_img)
                 lhpts_real, rhpts_real = hand_utils.get_keypoints(real_img)
                 hand_utils.display_hand_skleton(gen_img, lhpts_gen, rhpts_gen)
