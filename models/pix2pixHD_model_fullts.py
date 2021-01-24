a948f1ff6b4b761481c9ee71c3dfe474bcc8ed58
janmesh-007 <janmesh007@icloud.com>
2021-01-24 16:50:42 +0530

    inference

--- models/pix2pixHD_model_fullts.py
+++ models/pix2pixHD_model_fullts.py
@@ -331,10 +331,10 @@ class Pix2PixHDModel(BaseModel):
         I_0 = 0
         # Fake Generation
         
-        data_transforms = transforms.Compose(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
+        data_transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         
-        real_handsk_tensor = torch.tensor(cv2.cvtColor(real_handsk.copy(), cv2.COLOR_BGR2RGB), dtype=torch.float)
-        real_handsk_tensor = data_transforms(real_handsk_tensor.view(real_handsk.shape[2], real_handsk.shape[0], real_handsk.shape[1]))
+        real_handsk_pil = Image.fromarray(cv2.cvtColor(real_handsk.copy(), cv2.COLOR_BGR2RGB)).convert('RGB')
+        real_handsk_tensor = data_transforms(real_handsk_pil)
         real_handsk_tensor = real_handsk_tensor.view(1, real_handsk.shape[2], real_handsk.shape[0], real_handsk.shape[1]).cuda()
 
         input_concat = torch.cat((input_label, prevouts, real_handsk_tensor), dim=1) 
