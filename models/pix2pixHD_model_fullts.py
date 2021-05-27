9cd4bc4d384df14389a2c5229ba4482e353ff4e8
janmesh-007 <janmeshukey@gmail.com>
2021-05-28 00:36:53 +0530

    Update pix2pixHD_model_fullts.py

--- models/pix2pixHD_model_fullts.py
+++ models/pix2pixHD_model_fullts.py
@@ -269,7 +269,7 @@ class Pix2PixHDModel(BaseModel):
         I_hand_left_1 = torch.zeros(input_label.shape[0], input_label.shape[1], bbox_size, bbox_size).cuda()
         I_hand_right_1 = torch.zeros(input_label.shape[0], input_label.shape[1], bbox_size, bbox_size).cuda()
         if self.opt.shand_gen:
-            initial_I_1 = self.netG.forward(input_concat)
+            initial_I_1 = self.netG.forward(input_concat1)
             _hand_left_0 = torch.zeros(I_0.shape[0], I_0.shape[1], bbox_size, bbox_size).cuda()
             if next_hand_bbox[0] != 0 and next_hand_bbox[1]:
                 _hand_left_0[:,:,:hand_size_left_0[0],:hand_size_left_0[1]] = I_0[:, :, next_hand_bbox[1]:next_hand_bbox[1]+bbox_size, next_hand_bbox[0]:next_hand_bbox[0]+bbox_size]
@@ -277,7 +277,7 @@ class Pix2PixHDModel(BaseModel):
             if next_hand_bbox[2] != 0 and next_hand_bbox[3] != 0:    
                 _hand_right_0[:,:,:hand_size_right_0[0],:hand_size_right_0[1]] = I_0[:, :, next_hand_bbox[3]:next_hand_bbox[3]+bbox_size, next_hand_bbox[2]:next_hand_bbox[2]+bbox_size]
             I_hand_left_1 = self.shandGen.forward(torch.cat((hand_label_left_1, _hand_left_0), dim=1))
-            I_hand_right_1 = self.shandGen.forward(torch.cat((hand_label_right_1, _hand_right_0)), dim=1))
+            I_hand_right_1 = self.shandGen.forward(torch.cat((hand_label_right_1, _hand_right_0), dim=1))
             I_1 = initial_I_1.clone()
             if next_hand_bbox[0] != 0 and next_hand_bbox[1]:
                 I_1[:, :, next_hand_bbox[1]:next_hand_bbox[1]+bbox_size, next_hand_bbox[0]:next_hand_bbox[0]+bbox_size] = I_hand_left_1[:,:,:hand_size_left_1[0],:hand_size_left_1[1]]
