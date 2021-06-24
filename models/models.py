4c93d2748694e36fb96dcf56b1755ff41f93beff
janmesh-007 <janmeshukey@gmail.com>
2021-06-24 16:01:07 +0530

    Update models.py

--- models/models.py
+++ models/models.py
@@ -19,29 +19,29 @@ def create_model_refine(opt):
     model.initialize(opt)
     print("model [%s] was created" % (model.name()))
 
-    if opt.isTrain and len(opt.gpu_ids):
-        model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
+    # if opt.isTrain and len(opt.gpu_ids):
+    #     model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
 
     return model
 
-def create_model_D(opt):
-    from .pix2pixHD_model_D import Pix2PixHDModel
-    model = Pix2PixHDModel()    
-    model.initialize(opt)
-    print("model [%s] was created" % (model.name()))
+# def create_model_D(opt):
+#     from .pix2pixHD_model_D import Pix2PixHDModel
+#     model = Pix2PixHDModel()    
+#     model.initialize(opt)
+#     print("model [%s] was created" % (model.name()))
 
-    if opt.isTrain and len(opt.gpu_ids):
-        model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
+#     if opt.isTrain and len(opt.gpu_ids):
+#         model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
 
-    return model
+#     return model
 
-def create_model_flowD(opt):
-    from .pix2pixHD_model_flowD import Pix2PixHDModel
-    model = Pix2PixHDModel()    
-    model.initialize(opt)
-    print("model [%s] was created" % (model.name()))
+# def create_model_flowD(opt):
+#     from .pix2pixHD_model_flowD import Pix2PixHDModel
+#     model = Pix2PixHDModel()    
+#     model.initialize(opt)
+#     print("model [%s] was created" % (model.name()))
 
-    if opt.isTrain and len(opt.gpu_ids):
-        model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
+#     if opt.isTrain and len(opt.gpu_ids):
+#         model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
 
-    return model
+#     return model
